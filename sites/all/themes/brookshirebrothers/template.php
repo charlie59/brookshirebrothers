bbd7cb0961b7341b152522fc77ae71bc9a08b5ce
Charlie Llewellin <charlie@llewellin.net>
2016-10-13 16:52:25 -0500

    preprocess views

--- sites/all/themes/brookshirebrothers/template.php
+++ sites/all/themes/brookshirebrothers/template.php
@@ -447,8 +447,8 @@ function brookshirebrothers_preprocess_views_view(&$vars) {
     $view->build_info['substitutions'][$key] = $filtered;
   }
 
-  // $filtered = filter_xss($view->header);
-  $view->header = $filtered;
+  $filtered = filter_xss($vars['header']);
+  $vars['header'] = $filtered;
 
   dpm($vars);
 
