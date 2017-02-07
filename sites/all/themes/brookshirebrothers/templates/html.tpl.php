2cb30d0593a4cff3013588159731686210ed3ae8
Charlie Llewellin <charlie@llewellin.net>
2017-02-07 09:07:51 -0600

    remove call to jquery 1.8.3

--- sites/all/themes/brookshirebrothers/templates/html.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/html.tpl.php
@@ -54,8 +54,6 @@ $weekly_ad_array=Array();
      }
   </script>
   <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600,700,900,400italic|Roboto+Condensed:400,300,700|Open+Sans' rel='stylesheet' type='text/css'>
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-  <script type="text/javascript">window.jQuery || document.write('<script src="<?php echo url(path_to_theme(), array('absolute' => TRUE)); ?>/js/jquery-1.8.3.min.js"><\/script>')</script>
   <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=true&libraries=geometry"></script>
   <?php print $styles; ?>
   <!--[if lt IE 9]><link rel="stylesheet" type="text/css" href="<?php echo url(path_to_theme(), array('absolute' => TRUE)); ?>/ie.css" /><![endif]-->
