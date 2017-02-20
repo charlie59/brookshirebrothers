c34e05f9731f8bde66d7da9027ccce61a85fbcfc
Charlie Llewellin <charlie@llewellin.net>
2017-02-20 14:10:47 -0600

    cookie for store-locator

--- sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
@@ -1,4 +1,14 @@
-<p><input type="checkbox" id="default_store_id"> Set this store as your default</p>
+
+
+<p><input type="checkbox" id="default_store_id"<?php
+
+  if ( (isset($_COOKIE['defaultStore'])) && ($_COOKIE['defaultStore'] > 0)) {
+      echo ' checked="checked"> This store is your default';
+    }  else {
+    echo '> Set this store as your default';
+  }
+
+?></p>
 
 
 <iframe src="https://mydigitalpublication.com/publication/?pid=142&co=US&pc=<?php echo $_GET['store']; ?>" width="100%" height="1200px" frameborder="0"></iframe>
