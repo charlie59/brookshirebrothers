5270f45076888e83925b4e03f3606947f1bcd812
Charlie Llewellin <charlie@llewellin.net>
2017-02-20 12:47:08 -0600

    cookie for store-locator

--- sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
@@ -1,1 +1,4 @@
-test test<iframe src="https://mydigitalpublication.com/publication/?pid=142&co=US&pc=<?php echo $_GET['store']; ?>" width="100%" height="1200px" frameborder="0"></iframe>
+<input type="checkbox" id="store_id">
+
+
+<iframe src="https://mydigitalpublication.com/publication/?pid=142&co=US&pc=<?php echo $_GET['store']; ?>" width="100%" height="1200px" frameborder="0"></iframe>
