68ccc5fa680b03d22f491915db9ec421f77a4fdc
Charlie Llewellin <charlie@llewellin.net>
2017-02-20 19:41:47 -0600

    cookie for store-locator

--- sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
@@ -1,4 +1,4 @@
-
+<section class="text-block">
 
 <p><input type="checkbox" id="default_store_id"<?php
 
@@ -10,19 +10,19 @@
 
 ?></p>
 
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $("#default_store_id").change(function() {
+                if ($(this).is(":checked")) {
+                    var now = new Date();
+                    now.setTime(now.getTime() + 1 * 3600 * 1000 * 24 * 360 * 10);
+                    document.cookie = "defaultStore=" + "<?php echo $_GET['store']; ?>;" + " expires=" + now.toUTCString() + "; path=/;";
+                } else {
+                    document.cookie = "defaultStore=" + ";path=/;";
+                }
+            });
+        }(jQuery));
+    </script>
+</section>
 
 <iframe src="https://mydigitalpublication.com/publication/?pid=142&co=US&pc=<?php echo $_GET['store']; ?>" width="100%" height="1200px" frameborder="0"></iframe>
-
-<script type="text/javascript">
-    $(document).ready(function() {
-        $("#default_store_id").change(function() {
-            if ($(this).is(":checked")) {
-                var now = new Date();
-                now.setTime(now.getTime() + 1 * 3600 * 1000 * 24 * 360 * 10);
-                document.cookie = "defaultStore=" + "<?php echo $_GET['store']; ?>;" + " expires=" + now.toUTCString() + "; path=/;";
-            } else {
-                document.cookie = "defaultStore=" + ";path=/;";
-            }
-        });
-    }(jQuery));
-</script>
