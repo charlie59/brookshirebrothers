30fa04c5d9c18661d55c1b3f0f45f1e614cb0919
Charlie Llewellin <charlie@llewellin.net>
2017-04-11 12:20:27 -0500

    drop downs

--- sites/all/themes/brookshirebrothers/templates/includes/header.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/includes/header.tpl.php
@@ -6,41 +6,10 @@
                     src="<?php echo url(path_to_theme(), array('absolute' => TRUE)); ?>/images/logo.png"
                     alt="" width="290" height="62"></strong>
 
-        <div id="block-search-form" style="float: right;"
-             class="block block-search clearfix">
-
-            <form action="" method="post"
-                  id="search-block-form" accept-charset="UTF-8" _lpchecked="1">
-                <div>
-                    <div class="container-inline">
-                        <h2 class="element-invisible">Search form</h2>
-                        <div class="form-item form-type-textfield form-item-search-block-form">
-                            <label class="element-invisible"
-                                   for="edit-search-block-form--2">Search </label>
-                            <input title="Enter the terms you wish to search for."
-                                   type="text" id="edit-search-block-form--2"
-                                   name="search_block_form" value="" size="15"
-                                   maxlength="128" class="form-text">
-                        </div>
-                        <div class="form-actions form-wrapper"
-                             id="edit-actions"><input type="submit"
-                                                      id="edit-submit" name="op"
-                                                      value="Search"
-                                                      class="form-submit"></div>
-                        <input type="hidden" name="form_build_id"
-                               value="form-PABRcuL-imX8Zn97rV-62Mea9w77AGRyMbhWhvFi_V8">
-                        <input type="hidden" name="form_token"
-                               value="TVZJFiDMVBfJ4yXiCpLC3HfEgCSPtXE5zvRrJdGOhBY">
-                        <input type="hidden" name="form_id"
-                               value="search_block_form">
-                    </div>
-                </div>
-            </form>
-        </div>
+      <?php print render($page['header']); ?>
     </div>
   <?php $block = block_load('menu_block', '4');
   $block = _block_render_blocks(array($block));
   $block = _block_get_renderable_array($block);
   print drupal_render($block); ?>
-  <?php //print render($page['header']); ?>
 </header>
