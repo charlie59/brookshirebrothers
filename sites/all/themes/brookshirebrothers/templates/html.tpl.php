ff4fb0e9a9f41173bdffbc9b8477d837f5037efb
Charlie Llewellin <charlie@llewellin.net>
2017-06-25 18:33:57 -0500

    store-locator

--- sites/all/themes/brookshirebrothers/templates/html.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/html.tpl.php
@@ -127,6 +127,20 @@
         <script type="text/javascript" src="https://6311223.collect.igodigital.com/collect.js"></script>
         <script type="text/javascript"> _etmc.push(["setOrgId", "6311223"]);
             _etmc.push(["trackPageView"]); </script>
+        <script>
+            function initMap() {
+                var uluru = {lat: -25.363, lng: 131.044};
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 4,
+                    center: uluru
+                });
+                var marker = new google.maps.Marker({
+                    position: uluru,
+                    map: map
+                });
+            }
+        </script>
     </body>
     </html>
 <?php endif; ?>
+
