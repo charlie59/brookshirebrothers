f4b20965d585cb6ba3bea8cc2618a9b6ac3319c7
Charlie Llewellin <charlie@llewellin.net>
2017-10-20 16:00:56 -0500

    issue

--- sites/all/themes/brookshirebrothers/js/store-locator.js
+++ sites/all/themes/brookshirebrothers/js/store-locator.js
@@ -303,15 +303,16 @@ jQuery(document).ready(function () {
 
                 jQuery.each(semiresultsArray, function (ind, obj) {
                     var intermediateArr = jQuery.trim(semiresultsArray[ind].features[0].keywords.split(',')).split(',');
+                    console.log(intermediateArr);
                     jQuery.each(intermediateArr, function (el, key) {
-                        key = jQuery.trim(key)
-                        console.log(key);
+                        key = jQuery.trim(key);
+                        // console.log(key);
                         jQuery.each(checkboxArray, function (el2, key2) {
-                            console.log(key2);
+                            // console.log(key2);
                             key2 = jQuery.trim(key2);
                             //if (key.toLowerCase() === key2.toLowerCase()) {
                                 // console.log(obj);
-                                resultsArray.push(obj);
+                                // resultsArray.push(obj);
                                 return true
                             //} else {
                             //  return false;
