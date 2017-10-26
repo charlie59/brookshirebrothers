83dc0233d2d91574d49bcbf8d8baee76819635e9
Charlie Llewellin <charlie@llewellin.net>
2017-10-26 14:24:29 -0500

    fix php warning about undefined index

--- sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
@@ -50,4 +50,6 @@ if (isset($_COOKIE['defaultStore'])) {
     </script>
 </section>
 
+<?php if (isset($_GET['store'])): ?>
 <iframe src="https://mydigitalpublication.com/publication/?pid=142&co=US&pc=<?php echo $_GET['store']; ?>" width="100%" height="1200px" frameborder="0"></iframe>
+<?php endif; ?>
