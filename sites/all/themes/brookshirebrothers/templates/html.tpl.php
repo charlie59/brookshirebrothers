d9a5454bfccdc0101be07b608593f957f82e6cf6
Charlie Llewellin <charlie@llewellin.net>
2018-02-26 09:21:10 -0500

    facebook image

--- sites/all/themes/brookshirebrothers/templates/html.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/html.tpl.php
@@ -44,7 +44,7 @@
         <meta charset="utf-8">
         <meta name="viewport"
               content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-        <meta name="format-detection" content="telephone=no"
+        <meta name="format-detection" content="telephone=no">
       <?php print $head; ?>
         <title><?php print $head_title; ?></title>
         <script type="text/javascript">
