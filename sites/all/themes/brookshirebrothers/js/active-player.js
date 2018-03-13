43a26d67d8e09e786ec10d026adffb4b2ce902bb
Charlie Llewellin <charlie@llewellin.net>
2018-03-12 20:31:30 -0500

    changes to active-player.js

--- sites/all/themes/brookshirebrothers/js/active-player.js
+++ sites/all/themes/brookshirebrothers/js/active-player.js
@@ -1,48 +1,48 @@
-$(window).load(function() {
-removeSelected();
-$("#player-window-bg iframe").attr("src", $.trim($(".views-row-1 .views-field-field-webisode-embed-code").html())+"?autoplay=0");
-$(".views-row-1").addClass("selected");
-
-$(".webisodes-list .views-row a").on("click", function() {
+$( document ).ready(function() {
   removeSelected();
-  var ref=$(this);
-  $("#player-window-bg iframe").attr("src", $.trim($(this).parent().find(".views-field-field-webisode-embed-code").html())+"?autoplay=1");
-  ref.parent().addClass("selected");
+  $("#player-window-bg iframe").attr("src", $.trim($(".views-row-1 .views-field-field-webisode-embed-code").html()) + "?autoplay=0");
+  $(".views-row-1").addClass("selected");
+
+  $(".webisodes-list .views-row a").on("click", function () {
+    removeSelected();
+    var ref = $(this);
+    $("#player-window-bg iframe").attr("src", $.trim($(this).parent().find(".views-field-field-webisode-embed-code").html()) + "?autoplay=1");
+    ref.parent().addClass("selected");
+  });
+
+  $("a.overlay-link").hover(function () {
+    $(this).parent().addClass("hover");
+  }, function () {
+    $(this).parent().removeClass("hover");
+  });
+
 });
 
-$("a.overlay-link").hover(function(){
-  $(this).parent().addClass("hover");
-},function(){
-  $(this).parent().removeClass("hover");
-});
+(function ($) {
+  $(document).ajaxComplete(function () {
+    removeSelected();
+    $("#player-window-bg iframe").attr("src", $.trim($(".views-row-1 .views-field-field-webisode-embed-code").html()) + "?autoplay=1");
+    $(".views-row-1").addClass("selected");
+  });
 
-});
+  $(document).on("click", ".webisodes-list .views-row a", function () {
+    removeSelected();
+    var ref = $(this);
+    $("#player-window-bg iframe").attr("src", $.trim($(this).parent().find(".views-field-field-webisode-embed-code").html()) + "?autoplay=1");
+    ref.parent().addClass("selected");
+  });
 
-(function($) {
-$(document).ajaxComplete(function() {
-removeSelected();
-$("#player-window-bg iframe").attr("src", $.trim($(".views-row-1 .views-field-field-webisode-embed-code").html())+"?autoplay=1");
-$(".views-row-1").addClass("selected");
-});
-
-$(document).on("click",".webisodes-list .views-row a", function() {
-removeSelected();
-  var ref=$(this);
-  $("#player-window-bg iframe").attr("src", $.trim($(this).parent().find(".views-field-field-webisode-embed-code").html())+"?autoplay=1");
-  ref.parent().addClass("selected");
-});
-
-$(document).on({
+  $(document).on({
     mouseenter: function () {
-        $(this).parent().addClass("hover");
+      $(this).parent().addClass("hover");
     },
     mouseleave: function () {
-        $(this).parent().removeClass("hover");
+      $(this).parent().removeClass("hover");
     }
-}, "a.overlay-link");
+  }, "a.overlay-link");
 
 }(jQuery));
 
-function removeSelected(){
+function removeSelected() {
   $(".view-content .views-row").removeClass("selected");
 }
