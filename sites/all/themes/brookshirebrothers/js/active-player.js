e23cccc938a0ff5f362eccc65abbf5cdbee8a869
Charlie Llewellin <charlie@llewellin.net>
2018-03-13 09:13:09 -0500

    changes to active-player.js

--- sites/all/themes/brookshirebrothers/js/active-player.js
+++ sites/all/themes/brookshirebrothers/js/active-player.js
@@ -1,24 +1,25 @@
-$(document).ready(function() {
-  removeSelected();
-  $("#player-window-bg iframe").attr("src", $.trim($(".views-row-1 .views-field-field-webisode-embed-code").html()) + "?autoplay=0");
-  $(".views-row-1").addClass("selected");
-
-  $(".webisodes-list .views-row a").on("click", function () {
-    removeSelected();
-    var ref = $(this);
-    $("#player-window-bg iframe").attr("src", $.trim($(this).parent().find(".views-field-field-webisode-embed-code").html()) + "?autoplay=1");
-    ref.parent().addClass("selected");
-  });
-
-  $("a.overlay-link").hover(function () {
-    $(this).parent().addClass("hover");
-  }, function () {
-    $(this).parent().removeClass("hover");
-  });
-
-}(jQuery));
-
 (function ($) {
+
+  $(document).ready(function() {
+    removeSelected();
+    $("#player-window-bg iframe").attr("src", $.trim($(".views-row-1 .views-field-field-webisode-embed-code").html()) + "?autoplay=0");
+    $(".views-row-1").addClass("selected");
+
+    $(".webisodes-list .views-row a").on("click", function () {
+      removeSelected();
+      var ref = $(this);
+      $("#player-window-bg iframe").attr("src", $.trim($(this).parent().find(".views-field-field-webisode-embed-code").html()) + "?autoplay=1");
+      ref.parent().addClass("selected");
+    });
+
+    $("a.overlay-link").hover(function () {
+      $(this).parent().addClass("hover");
+    }, function () {
+      $(this).parent().removeClass("hover");
+    });
+
+  });
+
   $(document).ajaxComplete(function () {
     removeSelected();
     $("#player-window-bg iframe").attr("src", $.trim($(".views-row-1 .views-field-field-webisode-embed-code").html()) + "?autoplay=1");
