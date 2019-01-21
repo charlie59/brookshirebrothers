845f37ecb1e51ab5330ef1ec2272467e238a7a88
Charlie Llewellin <charlie@llewellin.net>
2019-01-21 17:45:03 -0500

    store redirect

--- sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
@@ -10,7 +10,7 @@ if (isset($_GET['store'])) {
   $result = $query->execute();
   $item_nid = array_keys($result['node']);
   $item = entity_load('node', $item_nid);
-  var_dump($item[0]);
+  var_dump($item[$item_nid]->field_weekly_ad_link);
   $link = $item->field_weekly_ad_link['und'][0]['value'];
   echo $link;
   exit;
