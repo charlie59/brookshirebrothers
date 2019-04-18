307db568fb4930b9b9e6ad152fb0f26e8037cf3a
Charlie Llewellin <charlie@llewellin.net>
2019-04-18 08:02:46 -0500

    fix redirect

--- sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/node/node--1917.tpl.php
@@ -16,12 +16,11 @@ if (isset($_GET['store'])) {
   $result = $query->execute();
   $item_nid = array_keys($result['node']);
   $nid = $item_nid[0];
-  echo $nid;
+  // echo $nid;
   if (!empty($nid)) {
     $item = entity_load('node', $item_nid);
     $link = $item["$nid"]->field_weekly_ad_link['und'][0]['value'];
-    echo $link;
-    exit;
+    // echo $link;
     ?>
       <script type="text/javascript">
           window.location.href = "<?php echo $link; ?>"
