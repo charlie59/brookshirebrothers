6c113bab154ae3e4f798f3fd1ed7187673541b08
Charlie Llewellin <charlie@llewellin.net>
2019-11-08 13:42:57 -0500

    adding facebook pixel

--- sites/all/themes/brookshirebrothers/templates/html.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/html.tpl.php
@@ -138,6 +138,23 @@ if (isset($_GET['ajax']) and $_GET['ajax'] == 1):
             gtag('config', 'DC-6360601');
         </script>
         <!-- End of global snippet: Please do not remove -->
+        <!-- Facebook Pixel Code added 11/8/2019-->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '785070881939094');
+            fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+                       src="https://www.facebook.com/tr?id=785070881939094&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Facebook Pixel Code -->
     </head>
     <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
     <div id="loader" class="loader" style=""></div>
