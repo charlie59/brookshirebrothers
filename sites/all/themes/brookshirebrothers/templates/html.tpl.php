e56029ee42f9ed7ee714baa518a8090b36b9c6d1
Charlie Llewellin <charlie@llewellin.net>
2020-04-15 16:34:53 -0500

    add hotjar script manually

--- sites/all/themes/brookshirebrothers/templates/html.tpl.php
+++ sites/all/themes/brookshirebrothers/templates/html.tpl.php
@@ -80,6 +80,7 @@ if (isset($_GET['ajax']) and $_GET['ajax'] == 1):
         <link rel="stylesheet" type="text/css" href="<?php echo url(path_to_theme(), ['absolute' => TRUE]); ?>/ie.css"/><![endif]-->
         <!--[if IE]>
         <script type="text/javascript" src="<?php echo url(path_to_theme(), ['absolute' => TRUE]); ?>/js/ie.js"></script><![endif]-->
+        <script type="text/javascript">
 <!--//--><![CDATA[//><!--
 (function(h,o,t,j,a,r){
   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
@@ -89,7 +90,7 @@ if (isset($_GET['ajax']) and $_GET['ajax'] == 1):
   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
   a.appendChild(r);
 })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-//--><!]]>
+//--><!]]></script>
       <?php print $scripts; ?>
         <script>
           (function (i, s, o, g, r, a, m) {
